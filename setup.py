3da575d2d803f7541f0641fc0ef8d9db665b9ab2
Gokberk Yaltirakli <git@gkbrk.com>
2020-04-20 12:47:26 +0100

    Upload package to PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="Rulid",
+    py_modules=["rulid"],
+    entry_points={"console_scripts": ["rulid=rulid:main"]},
+    version="0.0.2",
+    description="Build system and package manager for Rust",
+    author="Gokberk Yaltirakli",
+    author_email="opensource@gkbrk.com",
+    url="https://github.com/gkbrk/rulid",
+    keywords=["rust", "build", "package"],
+    license="GPL-3",
+)
